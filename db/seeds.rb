b6c7a52fcbc13d3cfdd65b1bb0dcd89923edb034
Ahluzain Octuviadi <ahloct11@gmail.com>
2023-11-11 23:47:49 +0700

    setup guten tag & remove skill field from job table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,37 +7,37 @@
 #   Character.create(name: "Luke", movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-company1 = Company.create!(name: "Sunscrapers", image: "https://public.justjoin.it/offers/company_logos/original_x2/fdb4ead4944104451e43b69865aff6398ad43d30.png?1698137026", country: "Berlin", hq_location: "123")
-Job.create!(title: "Team Lead Engineer (on-site)", salary_min: 32000, salary_max: 38000, salary_is_undisclosed: false,
+company1  = Company.create!(name: "Sunscrapers", image: "https://public.justjoin.it/offers/company_logos/original_x2/fdb4ead4944104451e43b69865aff6398ad43d30.png?1698137026", country: "Berlin", hq_location: "123")
+job1      = Job.create!(title: "Team Lead Engineer (on-site)", salary_min: 32000, salary_max: 38000, salary_is_undisclosed: false,
             employment_type: 0, location_type: 0, is_new: true, experience_level: 2, type_of_work: 0, job_description: "Text",
-            apply_link: "link", main_technology: "Python", online_interview: true,
-            skills: ["Python", "Golang", "Team Leadership", "AWS", "Docker"],
-            company_id: company1.id)
+            apply_link: "link", main_technology: "Python", online_interview: true, company_id: company1.id)
+job1.tag_names += ["Python", "Golang", "Team Leadership", "AWS", "Docker"]
+job1.save
 
-company2 = Company.create!(name: "Lingaro", image: "https://public.justjoin.it/offers/company_logos/original_x2/30764644353fa68f201a068dfed841f4b6895eb3.png?1697534145", country: "krakow", hq_location: "123")
-Job.create!(title: "Data Integration Engineer with GCP", salary_min: 0, salary_max: 0, salary_is_undisclosed: true,
+company2  = Company.create!(name: "Lingaro", image: "https://public.justjoin.it/offers/company_logos/original_x2/30764644353fa68f201a068dfed841f4b6895eb3.png?1697534145", country: "krakow", hq_location: "123")
+job2      = Job.create!(title: "Data Integration Engineer with GCP", salary_min: 0, salary_max: 0, salary_is_undisclosed: true,
             employment_type: 0, location_type: 2, is_new: true, experience_level: 1, type_of_work: 0, job_description: "Text",
-            apply_link: "link", main_technology: "Python", online_interview: true,
-            skills: ["GCP", "SQL", "Python"],
-            company_id: company2.id)
+            apply_link: "link", main_technology: "Python", online_interview: true, company_id: company2.id)
+job2.tag_names += ["GCP", "SQL", "Python"]
+job2.save
 
-company3 = Company.create!(name: "Adverity", image: "https://public.justjoin.it/offers/company_logos/original_x2/d9e98ccd1eba844a04f9ba4b8e6d0f9414d87fe1.png?1697791668", country: "Katowice", hq_location: "123")
-Job.create!(title: "Junior Full Stack Engineer", salary_min: 2900, salary_max: 3750, salary_is_undisclosed: false,
+company3  = Company.create!(name: "Adverity", image: "https://public.justjoin.it/offers/company_logos/original_x2/d9e98ccd1eba844a04f9ba4b8e6d0f9414d87fe1.png?1697791668", country: "Katowice", hq_location: "123")
+job3      = Job.create!(title: "Junior Full Stack Engineer", salary_min: 2900, salary_max: 3750, salary_is_undisclosed: false,
             employment_type: 0, location_type: 0, is_new: true, experience_level: 0, type_of_work: 1, job_description: "Text",
-            apply_link: "link", main_technology: "Python", online_interview: true,
-            skills: ["Python", "Golang", "React", "Typescript", "Rest API"],
-            company_id: company3.id)
+            apply_link: "link", main_technology: "Python", online_interview: true, company_id: company3.id)
+job3.tag_names += ["Python", "Golang", "React", "Typescript", "Rest API"]
+job3.save
 
-company4 = Company.create!(name: "Fujitsu Technology Solution", image: "https://public.justjoin.it/offers/company_logos/original_x2/9bc6a99b9877a41d625a0e1f2facf85854528488.jpg?1697190866", country: "Warszawa", hq_location: "123")
-Job.create!(title: "IAM Automation Engineer)", salary_min: 0, salary_max: 0, salary_is_undisclosed: true,
+company4  = Company.create!(name: "Fujitsu Technology Solution", image: "https://public.justjoin.it/offers/company_logos/original_x2/9bc6a99b9877a41d625a0e1f2facf85854528488.jpg?1697190866", country: "Warszawa", hq_location: "123")
+job4      = Job.create!(title: "IAM Automation Engineer)", salary_min: 0, salary_max: 0, salary_is_undisclosed: true,
             employment_type: 0, location_type: 2, is_new: true, experience_level: 2, type_of_work: 0, job_description: "Text",
-            apply_link: "link", main_technology: "Java", online_interview: false,
-            skills: ["Java", "Web Services", "SQL", "BeanShell", "IAM Concepts"],
-            company_id: company4.id)
+            apply_link: "link", main_technology: "Java", online_interview: false, company_id: company4.id)
+job4.tag_names += ["Java", "Web Services", "SQL", "BeanShell", "IAM Concepts"]
+job4.save
 
-company5 = Company.create!(name: "Intent", image: "https://public.justjoin.it/offers/company_logos/original_x2/e95737b5eecb9ecc33c85643703f922b4d05cf68.png?1697531283", country: "Gdansk", hq_location: "123")
-Job.create!(title: "Junior Flutter Developer", salary_min: 9000, salary_max: 13000, salary_is_undisclosed: false,
+company5  = Company.create!(name: "Intent", image: "https://public.justjoin.it/offers/company_logos/original_x2/e95737b5eecb9ecc33c85643703f922b4d05cf68.png?1697531283", country: "Gdansk", hq_location: "123")
+job5      = Job.create!(title: "Junior Flutter Developer", salary_min: 9000, salary_max: 13000, salary_is_undisclosed: false,
             employment_type: 0, location_type: 2, is_new: true, experience_level: 0, type_of_work: 0, job_description: "Text",
-            apply_link: "link", main_technology: "Mobile", online_interview: true,
-            skills: ["English", "Flutter", "Dart"],
-            company_id: company5.id)
+            apply_link: "link", main_technology: "Mobile", online_interview: true, company_id: company5.id)
+job5.tag_names += ["English", "Flutter", "Dart"]
+job5.save
