b5121dc314b3c625b76e6cd98f9273dff3e9e89b
Ahluzain Octuviadi <ahloct11@gmail.com>
2023-11-18 16:31:27 +0700

    implement search job input

--- app/assets/elm/Components/JobFilter.elm
+++ app/assets/elm/Components/JobFilter.elm
@@ -1,11 +1,14 @@
 module Components.JobFilter exposing (..)
 
 import Components.JobMoreFilter as JobMoreFilter
-import Components.JobSortDropDown as JobSortDropDown exposing (DropDownStateMsg(..))
+import Components.JobSearch as JobSearch exposing (InputGroupStateMsg(..))
+import Components.JobSortDropDown as JobSortDropDown
 import Components.MainTechnology exposing (MainTechnology, emptyMainTechnology, listMainTechnology)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import List exposing (isEmpty, length)
+import String exposing (fromInt)
 
 
 
@@ -13,9 +16,10 @@ import Html.Events exposing (onClick)
 
 
 type alias Model =
-    { jobSortDropDownModel : JobSortDropDown.Model
-    , jobMoreFilterModel : JobMoreFilter.Model
+    { jobSearchModel : JobSearch.Model
     , selectedMainTechnology : MainTechnology
+    , jobSortDropDownModel : JobSortDropDown.Model
+    , jobMoreFilterModel : JobMoreFilter.Model
     }
 
 
@@ -24,9 +28,10 @@ type alias Model =
 
 
 type Msg
-    = JobSortDropDownUpdateMsg JobSortDropDown.Msg
-    | JobMoreFilterUpdateMsg JobMoreFilter.Msg
+    = JobSearchUpdateMsg JobSearch.Msg
     | SelectMainTechnologyState MainTechnology
+    | JobSortDropDownUpdateMsg JobSortDropDown.Msg
+    | JobMoreFilterUpdateMsg JobMoreFilter.Msg
 
 
 
@@ -35,9 +40,10 @@ type Msg
 
 init : Model
 init =
-    { jobSortDropDownModel = JobSortDropDown.init
-    , jobMoreFilterModel = JobMoreFilter.init
+    { jobSearchModel = JobSearch.init
     , selectedMainTechnology = emptyMainTechnology
+    , jobSortDropDownModel = JobSortDropDown.init
+    , jobMoreFilterModel = JobMoreFilter.init
     }
 
 
@@ -48,6 +54,24 @@ init =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        JobSearchUpdateMsg msg_ ->
+            let
+                ( newJobSearchUpdate, _ ) =
+                    JobSearch.update msg_ model.jobSearchModel
+            in
+            ( { model | jobSearchModel = newJobSearchUpdate }, Cmd.none )
+
+        SelectMainTechnologyState msg_ ->
+            let
+                newModel =
+                    if model.selectedMainTechnology == msg_ then
+                        { model | selectedMainTechnology = emptyMainTechnology }
+
+                    else
+                        { model | selectedMainTechnology = msg_ }
+            in
+            ( newModel, Cmd.none )
+
         JobSortDropDownUpdateMsg msg_ ->
             let
                 ( newJobSortDropDownUpdate, _ ) =
@@ -62,25 +86,34 @@ update msg model =
             in
             ( { model | jobMoreFilterModel = newJobMoreFilterUpdate }, Cmd.none )
 
-        SelectMainTechnologyState msg_ ->
-            let
-                newSelectedMainTechnology =
-                    if model.selectedMainTechnology == msg_ then
-                        { model | selectedMainTechnology = emptyMainTechnology }
-
-                    else
-                        { model | selectedMainTechnology = msg_ }
-            in
-            ( newSelectedMainTechnology, Cmd.none )
-
 
 
 -- VIEW
 
 
 view : Model -> Html Msg
-view { jobSortDropDownModel, jobMoreFilterModel, selectedMainTechnology } =
+view { jobMoreFilterModel, jobSearchModel, jobSortDropDownModel, selectedMainTechnology } =
     let
+        searchButtonMobile =
+            a
+                [ class "flex items-center justify-center no-underline outline outline-slate-200 p-2 rounded-full hover:cursor-pointer md:hidden"
+                , if isEmpty jobSearchModel.listInputValue then
+                    class "bg-slate-100 text-slate-500"
+
+                  else
+                    class "bg-primary-1 text-white"
+                , onClick (JobSearchUpdateMsg (JobSearch.InputGroupState Open))
+                ]
+                [ div [ class "flex flex-row items-center" ]
+                    [ span [ class "fa-solid fa-magnifying-glass" ] []
+                    , if isEmpty jobSearchModel.listInputValue then
+                        span [] []
+
+                      else
+                        span [ class "pl-2 text-xs" ] [ text ("+" ++ fromInt (length jobSearchModel.listInputValue)) ]
+                    ]
+                ]
+
         listMainTechnologyView =
             listMainTechnology
                 |> List.map
@@ -110,68 +143,60 @@ view { jobSortDropDownModel, jobMoreFilterModel, selectedMainTechnology } =
                             ]
                     )
     in
-    div [ class "bg-white p-2 md:pt-2 md:pb-4" ]
-        [ div []
-            [ div [ class "md:grid md:grid-cols-3 md:gap-x-4" ]
-                [ div [ class "flex items-center overflow-x-scroll no-scrollbar p-1.5 gap-x-2 md:gap-x-4" ]
-                    [ div [ class "hidden relative md:block" ]
-                        [ div [ class "absolute flex inset-y-0 items-center left-0 pl-3 pointer-events-none" ]
-                            [ span [ class "text-gray-500 sm:text-sm" ]
-                                [ i [ class "fa-solid fa-magnifying-glass" ] []
+    div [ class "relative z-10" ]
+        [ div [ class "bg-white h-full" ]
+            [ div [ class "block md:hidden" ] [ Html.map JobSearchUpdateMsg <| JobSearch.slideOverView jobSearchModel ]
+            , div []
+                [ div [ class "flex px-2.5 justify-between" ]
+                    [ div [ class "flex items-center px-1.5 py-2.5 gap-x-2 md:gap-x-4 md:w-[400px] overflow-x-scroll md:overflow-x-visible" ]
+                        [ div [ class "hidden relative md:block" ]
+                            [ Html.map JobSearchUpdateMsg <| JobSearch.desktopView jobSearchModel
+                            ]
+                        , searchButtonMobile
+                        , a [ class "h-8 items-center no-underline outline outline-slate-200 px-4 rounded-3xl md:h-9 md:flex md:gap-x-2" ]
+                            [ div [ class "hidden items-center md:block" ]
+                                [ span [ class "fa-solid fa-location-dot text-slate-700" ] []
+                                ]
+                            , span [ class "text-sm text-slate-700" ] [ text "Location" ]
+                            , div [ class "hidden items-center md:flex" ]
+                                [ i [ class "fa-solid fa-chevron-down text-sm text-slate-700" ] []
                                 ]
                             ]
-                        , input
-                            [ placeholder "Search"
-                            , class "bg-slate-100 border-1 border-slate-200 h-10 pl-8 mt-2 rounded-3xl text-slate-500 w-full focus:outline-none placeholder:text-sm placeholder:text-slate-400"
+                        , a [ class "flex h-8 items-center no-underline outline outline-slate-200 rounded-3xl md:hidden" ]
+                            [ span [ class "px-4 text-sm text-slate-700 truncate" ] [ text "Tech" ]
                             ]
-                            []
-                        ]
-                    , a [ class "bg-slate-100 flex items-center justify-center no-underline outline outline-slate-200 p-2 rounded-full hover:cursor-pointer md:hidden" ]
-                        [ span [ class "fa-solid fa-magnifying-glass text-slate-500" ] []
-                        ]
-                    , a [ class "h-8 items-center no-underline outline outline-slate-200 px-4 rounded-3xl md:h-10 md:flex md:gap-x-2" ]
-                        [ div [ class "hidden items-center md:block" ]
-                            [ span [ class "fa-solid fa-location-dot text-slate-700" ] []
-                            ]
-                        , span [ class "text-sm text-slate-700" ] [ text "Location" ]
-                        , div [ class "hidden items-center md:flex" ]
-                            [ i [ class "fa-solid fa-chevron-down text-sm text-slate-700" ] []
-                            ]
-                        ]
-                    , a [ class "flex h-8 items-center no-underline outline outline-slate-200 rounded-3xl md:hidden" ]
-                        [ span [ class "px-4 text-sm text-slate-700 truncate" ] [ text "Tech" ]
-                        ]
-                    , a
-                        [ class "flex h-8 items-center no-underline outline outline-slate-200 rounded-3xl md:hidden"
-                        , onClick (JobMoreFilterUpdateMsg JobMoreFilter.ModalState)
-                        ]
-                        [ span [ class "px-4 text-sm text-slate-700 truncate" ] [ text "More filters" ]
-                        ]
-                    , div []
-                        [ a
+                        , a
                             [ class "flex h-8 items-center no-underline outline outline-slate-200 rounded-3xl md:hidden"
-                            , onClick (JobSortDropDownUpdateMsg JobSortDropDown.DropDownState)
-                            ]
-                            [ div [ class "px-4 text-sm text-slate-700 truncate" ] [ text "Sort by: Default" ]
-                            ]
-                        , Html.map JobSortDropDownUpdateMsg <| JobSortDropDown.dropDownView jobSortDropDownModel
-                        ]
-                    ]
-                , div [ class "hidden gap-x-4 md:col-span-2 md:flex md:justify-end" ]
-                    [ div [ class "flex flex-row gap-x-4 no-scrollbar overflow-x-scroll p-2" ]
-                        listMainTechnologyView
-                    , div [ class "flex items-center" ]
-                        [ a
-                            [ class "cursor-pointer h-8 items-center no-underline outline outline-slate-200 px-4 rounded-3xl w-40 md:flex md:gap-x-2 md:h-10"
                             , onClick (JobMoreFilterUpdateMsg JobMoreFilter.ModalState)
                             ]
-                            [ span [ class "fa-solid fa-sliders text-slate-700" ] []
-                            , span [ class "text-sm text-slate-700" ] [ text "More filters" ]
-                            , i [ class "fa-solid fa-chevron-down text-slate-700 text-sm" ] []
+                            [ span [ class "px-4 text-sm text-slate-700 truncate" ] [ text "More filters" ]
+                            ]
+                        , div []
+                            [ a
+                                [ class "flex h-8 items-center no-underline outline outline-slate-200 rounded-3xl md:hidden"
+                                , onClick (JobSortDropDownUpdateMsg JobSortDropDown.DropDownState)
+                                ]
+                                [ div [ class "px-4 text-sm text-slate-700 truncate" ] [ text "Sort by: Default" ]
+                                ]
+                            , Html.map JobSortDropDownUpdateMsg <| JobSortDropDown.dropDownView jobSortDropDownModel
+                            ]
+                        ]
+                    , div [ class "hidden gap-x-4 px-1 md:flex md:justify-end overflow-x-auto" ]
+                        [ div [ class "flex flex-row gap-x-4 no-scrollbar overflow-x-scroll p-2" ]
+                            listMainTechnologyView
+                        , div [ class "flex items-center" ]
+                            [ a
+                                [ class "cursor-pointer h-8 items-center no-underline outline outline-slate-200 px-4 rounded-3xl w-40 md:flex md:gap-x-2 md:h-10"
+                                , onClick (JobMoreFilterUpdateMsg JobMoreFilter.ModalState)
+                                ]
+                                [ span [ class "fa-solid fa-sliders text-slate-700" ] []
+                                , span [ class "text-sm text-slate-700" ] [ text "More filters" ]
+                                , i [ class "fa-solid fa-chevron-down text-slate-700 text-sm" ] []
+                                ]
                             ]
                         ]
                     ]
+                , Html.map JobMoreFilterUpdateMsg <| JobMoreFilter.view jobMoreFilterModel
                 ]
-            , Html.map JobMoreFilterUpdateMsg <| JobMoreFilter.view jobMoreFilterModel
             ]
         ]
